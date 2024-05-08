c2175a8aa35aedb5e8a93dcdb5c8e2a2bb96150e
Gabriel Ribeiro de Freitas <116754893+SrHut27@users.noreply.github.com>
2024-05-08 14:16:38 +0000

    atualização

--- server/configs/database.js
+++ server/configs/database.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2'); // importando o módul que conversa com o sistema de banco de dados mySql
 
 const connection = mysql.createConnection({
-    host: "192.168.15.149", // estava testando: você aluno fará "localhost"
-    user: "mp_gabriel",  // use o usuário padrão do computador, o "root"
-    password: "****************", // deixe em branco: ""
+    host: "localhost", // estava testando: você aluno fará "localhost"
+    user: "root",  // use o usuário padrão do computador, o "root"
+    password: "", // deixe em branco: ""
     database: "projeto_1" // esse é a tabela que usaremos, Vocês vão nomeá-la de Sistema_mp, no arquivo database.sql na pasta extras
 });
 
